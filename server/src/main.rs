97f7b38899ac4140a1a1007851eff48350d66467
sid-lucas <pro.github.rojvi@slmails.com>
2026-01-29 09:51:05 +0100

    Add initial server code with dependencies [axum,tokio,tower]

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+use tokio::{
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Test" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = TcpListener::bind("localhost:2727").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
