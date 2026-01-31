b14efdcaa790e05dfcc6b372b0e3a269b154a708
Dylan Ramos <github.5mwg9@simplelogin.com>
2026-01-31 14:06:13 +0100

    Change root

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,45 +1,23 @@
-use axum::{Json, extract::State};
+use crate::models::{CreateUserPayload, User};
+use axum::{Json, extract::State, http::StatusCode};
 use sqlx::PgPool;
 
-use crate::models::{CreateUserPayload, User};
-
-pub async fn root(State(pool): State<PgPool>) -> String {
-    let users = sqlx::query_as!(
-        User,
-        r#"
-        SELECT id, username
-        FROM users
-        ORDER BY id
-        LIMIT 100
-        "#
-    )
-    .fetch_all(&pool)
-    .await
-    .expect("Failed to fetch users");
-
-    users
-        .into_iter()
-        .map(|u| u.username)
-        .collect::<Vec<_>>()
-        .join(", ")
+pub async fn root() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "Dydyscord Server is running!")
 }
 
 pub async fn create_user(
     State(pool): State<PgPool>,
     Json(payload): Json<CreateUserPayload>,
-) -> Json<User> {
+) -> Result<(StatusCode, Json<User>), StatusCode> {
     let user = sqlx::query_as!(
         User,
-        r#"
-        INSERT INTO users (username)
-        VALUES ($1)
-        RETURNING id, username
-        "#,
+        "INSERT INTO users (username) VALUES ($1) RETURNING id, username",
         payload.username
     )
     .fetch_one(&pool)
     .await
-    .expect("Failed to create user");
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    Json(user)
+    Ok((StatusCode::CREATED, Json(user)))
 }
