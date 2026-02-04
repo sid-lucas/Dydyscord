a6b083e7e9deef972e75b31a540725fbe1dac1be
sid-lucas <pro.github.rojvi@slmails.com>
2026-02-04 21:48:13 +0100

    Modify mls storage module

--- client/src/mls/storage.rs
+++ client/src/mls/storage.rs
@@ -1,18 +1,37 @@
+use std::os::unix::fs::PermissionsExt;
 use std::{fs, path::PathBuf};
 
 pub struct FileBackedStorage {
     path: PathBuf,
 }
 
-fn ensure_parent_dir(path: &std::path::Path) {
-    if let Some(parent) = path.parent() {
-        fs::create_dir_all(parent).expect("create_dir_all failed");
+fn ensure_storage_path() -> PathBuf {
+    // chemin jusqu'au dossier
+    let mut dir = PathBuf::new();
+    dir.push("/home/.dydyscord");
+
+    // Créer le dossier de l'app si non existant
+    if !dir.exists() {
+        fs::create_dir_all(&dir).expect("Failed to create dir");
+        fs::set_permissions(&dir, fs::Permissions::from_mode(0o700)).unwrap();
     }
+
+    // chemin jusqu'au fichier secret
+    let mut file = dir.clone();
+    file.push(".secret");
+
+    // Créer le fichier si non existant
+    if !file.exists() {
+        fs::File::create(&file).expect("Failed to create file");
+        fs::set_permissions(&file, fs::Permissions::from_mode(0o600)).unwrap();
+    }
+
+    dir
 }
 
 impl FileBackedStorage {
-    pub fn new(path: PathBuf) -> Self {
-        ensure_parent_dir(&path);
+    pub fn new() -> Self {
+        let path = ensure_storage_path();
         Self { path }
     }
 
