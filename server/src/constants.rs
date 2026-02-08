1c8699230bda580d1bb8395bf0be8ac9109905f7
sid-lucas <pro.github.rojvi@slmails.com>
2026-02-08 09:50:17 +0100

    Move mod.rs in dedicated files (idiomatic way)

--- server/src/constants.rs
+++ server/src/constants.rs
@@ -2,3 +2,5 @@ pub const JWT_DURATION_MINUTES: i64 = 60;
 pub const AUTH_HEADER: &str = "auth-token";
 
 pub const SERVER_ADDR: &str = "0.0.0.0:3000";
+
+// TODO : Mettre var environnement ici aussi?
